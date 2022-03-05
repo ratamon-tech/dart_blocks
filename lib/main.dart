add211a1cf6a6dcbc15aacb08bb3769c6151959b
ratamon-tech <ratamon91@gmail.com>
2022-03-06 08:37:52 +0900

    layout challenge task

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.red,
+                child: Text('container1'),
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.blue,
+                child: Text('container1'),
+              )
+            ],
+          )
+        ),
       ),
     );
   }
